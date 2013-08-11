d2bcac21041884267bd4ef5ceec6ab1c3218f814
Jonas 'Sortie' Termansen <sortie@maxsi.org>
2013-08-11 21:41:29 +0200

    Fix case-sensitive path compatibility in utils/common/filesystem_tools.cpp.
    This fixes the case where the user wants to access a file in:
    
      /home/Foo/Steam/...
    
    but code converts that to lower-case and it instead accesses:
    
      /home/foo/steam

--- mp/src/utils/common/filesystem_tools.cpp
+++ mp/src/utils/common/filesystem_tools.cpp
@@ -62,7 +62,9 @@ void FileSystem_SetupStandardDirectories( const char *pFilename, const char *pGa
 
 	Q_MakeAbsolutePath( qdir, sizeof( qdir ), pFilename, NULL );
 	Q_StripFilename( qdir );
+#if defined(WIN32)
 	Q_strlower( qdir );
+#endif
 	if ( qdir[0] != 0 )
 	{
 		Q_AppendSlash( qdir, sizeof( qdir ) );
