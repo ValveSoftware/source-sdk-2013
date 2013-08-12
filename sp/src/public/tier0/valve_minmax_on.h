4bba2dc246a6e6dc688d3e257e41f5d9ff0165c2
Jonas 'Sortie' Termansen <sortie@maxsi.org>
2013-08-12 21:13:51 +0200

    Fix min and max not being redeclared on POSIX.
    Why was this not redeclared on POSIX? Am I breaking something?

--- sp/src/public/tier0/valve_minmax_on.h
+++ sp/src/public/tier0/valve_minmax_on.h
@@ -1,9 +1,7 @@
 //========= Copyright Valve Corporation, All rights reserved. ============//
-#if !defined(POSIX)
 #ifndef min
 	#define min(a,b)  (((a) < (b)) ? (a) : (b))
 #endif
 #ifndef max
 	#define max(a,b)  (((a) > (b)) ? (a) : (b))
-#endif
 #endif
