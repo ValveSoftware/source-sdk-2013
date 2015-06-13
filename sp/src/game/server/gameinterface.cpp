4595a9e036980f7a39496b6795551107cb9ac939
ZombieRoxtar <ZombieRoxtar@users.noreply.github.com>
2015-06-12 23:33:11 -0400

    Allowing multiple AdditionalContentId entries in gameinfo.txt

--- sp/src/game/server/gameinterface.cpp
+++ sp/src/game/server/gameinterface.cpp
@@ -565,6 +565,36 @@ EXPOSE_SINGLE_INTERFACE_GLOBALVAR(CServerGameDLL, IServerGameDLL, INTERFACEVERSI
 // When bumping the version to this interface, check that our assumption is still valid and expose the older version in the same way
 COMPILE_TIME_ASSERT( INTERFACEVERSION_SERVERGAMEDLL_INT == 9 );
 
+/* BM: https://developer.valvesoftware.com/wiki/Mounting_multiple_games */
+static void MountAdditionalContent()
+{
+	KeyValues *pMainFile = new KeyValues("gameinfo.txt");
+#ifndef _WINDOWS
+	// case sensitivity
+	pMainFile->LoadFromFile(filesystem, "GameInfo.txt", "MOD");
+	if (!pMainFile)
+#endif
+		pMainFile->LoadFromFile(filesystem, "gameinfo.txt", "MOD");
+
+	if (pMainFile)
+	{
+		KeyValues* pFileSystemInfo = pMainFile->FindKey("FileSystem");
+		if (pFileSystemInfo)
+			for (KeyValues *pKey = pFileSystemInfo->GetFirstSubKey(); pKey; pKey = pKey->GetNextKey())
+			{
+			if (strcmp(pKey->GetName(), "AdditionalContentId") == 0)
+			{
+				int appid = abs(pKey->GetInt());
+				if (appid)
+					if (filesystem->MountSteamContent(-appid) != FILESYSTEM_MOUNT_OK)
+						Warning("Unable to mount extra content with appId: %i\n", appid);
+			}
+			}
+	}
+	pMainFile->deleteThis();
+}
+//*/
+
 bool CServerGameDLL::DLLInit( CreateInterfaceFn appSystemFactory, 
 		CreateInterfaceFn physicsFactory, CreateInterfaceFn fileSystemFactory, 
 		CGlobalVars *pGlobals)
@@ -636,6 +666,9 @@ bool CServerGameDLL::DLLInit( CreateInterfaceFn appSystemFactory,
 	// Yes, both the client and game .dlls will try to Connect, the soundemittersystem.dll will handle this gracefully
 	if ( !soundemitterbase->Connect( appSystemFactory ) )
 		return false;
+
+	/* BM: Called it; see above */
+	MountAdditionalContent();
 
 	// cache the globals
 	gpGlobals = pGlobals;
