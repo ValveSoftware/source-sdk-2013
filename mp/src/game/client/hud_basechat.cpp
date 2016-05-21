b708588694d5fdaed48324eb735d0904f5cc7d36
TomyLobo <tomylobo@nurfuerspam.de>
2016-05-21 20:18:03 +0200

    Move cursor to the actual center of the chat box

--- mp/src/game/client/hud_basechat.cpp
+++ mp/src/game/client/hud_basechat.cpp
@@ -1203,7 +1203,7 @@ void CBaseHudChat::StartMessageMode( int iMessageModeType )
 
 	//Place the mouse cursor near the text so people notice it.
 	int x, y, w, h;
-	GetChatHistory()->GetBounds( x, y, w, h );
+	GetBounds( x, y, w, h );
 	vgui::input()->SetCursorPos( x + ( w/2), y + (h/2) );
 
 	m_flHistoryFadeTime = gpGlobals->curtime + CHAT_HISTORY_FADE_TIME;
