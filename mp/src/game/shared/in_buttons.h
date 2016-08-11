dceeaebfce9f7a092d2185200d918141c901e45e
Deyan Dobromirov <dvd_video@abv.bg>
2016-08-11 09:37:38 +0300

    Removed the nasty tab

--- mp/src/game/shared/in_buttons.h
+++ mp/src/game/shared/in_buttons.h
@@ -1,6 +1,6 @@
 //========= Copyright Valve Corporation, All rights reserved. ============//
 //
-// Purpose: 
+// Purpose:
 //
 // $NoKeywords: $
 //
@@ -36,6 +36,6 @@
 #define IN_BULLRUSH   (1 << 22)
 #define IN_GRENADE1   (1 << 23)	// grenade 1
 #define IN_GRENADE2   (1 << 24)	// grenade 2
-#define	IN_ATTACK3    (1 << 25)
+#define IN_ATTACK3    (1 << 25)
 
 #endif // IN_BUTTONS_H
