b594b5cff6f84092134d04315ac6cd4074108645
weaponcubemap <67855370+weaponcubemap@users.noreply.github.com>
2020-07-05 14:03:45 +0300

    Moved "Playing sequence" spam to Developer console

--- mp/src/game/client/c_baseanimating.cpp
+++ mp/src/game/client/c_baseanimating.cpp
@@ -3455,7 +3455,7 @@ void C_BaseAnimating::DoAnimationEvents( CStudioHdr *pStudioHdr )
 	if ( nSeqNum >= nStudioNumSeq )
 	{
 		// This can happen e.g. while reloading Heavy's shotgun, switch to the minigun.
-		Warning( "%s[%d]: Playing sequence %d but there's only %d in total?\n", GetDebugName(), entindex(), nSeqNum, nStudioNumSeq );
+		DevWarning( "%s[%d]: Playing sequence %d but there's only %d in total?\n", GetDebugName(), entindex(), nSeqNum, nStudioNumSeq );
 		return;
 	}
 
