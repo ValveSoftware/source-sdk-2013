ca46a49da55b0dabd4db498e4eeeb667af1913af
weaponcubemap <67855370+weaponcubemap@users.noreply.github.com>
2020-07-05 13:49:26 +0300

    Fixed crouch bug
    If you crouch then uncrouch and crouch before finishing uncrouch, would result to camera freeze

--- mp/src/game/shared/gamemovement.cpp
+++ mp/src/game/shared/gamemovement.cpp
@@ -4191,9 +4191,6 @@ void CGameMovement::FinishUnDuckJump( trace_t &trace )
 //-----------------------------------------------------------------------------
 void CGameMovement::FinishDuck( void )
 {
-	if ( player->GetFlags() & FL_DUCKING )
-		return;
-
 	player->AddFlag( FL_DUCKING );
 	player->m_Local.m_bDucked = true;
 	player->m_Local.m_bDucking = false;
