f91bb98255233d30f818fee27f78f9196f95b7ec
weaponcubemap <67855370+weaponcubemap@users.noreply.github.com>
2020-07-24 11:56:26 +0300

    Removing useless code from RPG part 2

--- mp/src/game/shared/hl2mp/weapon_rpg.h
+++ mp/src/game/shared/hl2mp/weapon_rpg.h
@@ -190,8 +190,6 @@ public:
 	bool	WeaponShouldBeLowered( void );
 	bool	Lower( void );
 
-	bool	CanHolster( void );
-
 	virtual void Drop( const Vector &vecVelocity );
 
 	int		GetMinBurst() { return 1; }
