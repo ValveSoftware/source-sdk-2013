cebc13da329811085d6bec428649eea385c15e29
weaponcubemap <67855370+weaponcubemap@users.noreply.github.com>
2020-07-29 16:37:19 +0300

    Fixed another missing effect in multiplayer

--- mp/src/game/server/hl2/hl2_player.cpp
+++ mp/src/game/server/hl2/hl2_player.cpp
@@ -3716,6 +3716,8 @@ const impactdamagetable_t &CHL2_Player::GetPhysicsImpactDamageTable()
 //-----------------------------------------------------------------------------
 void CHL2_Player::Splash( void )
 {
+	IPredictionSystem::SuppressHostEvents(NULL);
+	
 	CEffectData data;
 	data.m_fFlags = 0;
 	data.m_vOrigin = GetAbsOrigin();
