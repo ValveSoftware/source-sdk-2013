afc217e3dc60d9a11ac89c9ebb90ef62605782cb
zorggish <xenomat@Yandex.ru>
2020-09-14 11:56:35 +0300

    Fixing Parenting

--- sp/src/game/client/c_env_projectedtexture.cpp
+++ sp/src/game/client/c_env_projectedtexture.cpp
@@ -221,15 +221,14 @@ void C_EnvProjectedTexture::UpdateLight( bool bForceUpdate )
 
 	g_pClientShadowMgr->SetFlashlightLightWorld( m_LightHandle, m_bLightWorld );
 
-	if ( bForceUpdate == false )
-	{
+	//if ( bForceUpdate == false )
 		g_pClientShadowMgr->UpdateProjectedTexture( m_LightHandle, true );
-	}
 }
 
 void C_EnvProjectedTexture::Simulate( void )
 {
-	UpdateLight( false );
+	//UpdateLight( false );
+	UpdateLight( GetMoveParent() != NULL );
 
 	BaseClass::Simulate();
 }
