c7657b1547766630cb3160462ab4d495afe33f1b
Blixibon <acj3017@gmail.com>
2021-03-12 11:04:56 -0600

    Fixed a crash related to an obscure choreo event

--- sp/src/game/server/sceneentity.cpp
+++ sp/src/game/server/sceneentity.cpp
@@ -3461,7 +3461,10 @@ void CSceneEntity::StartEvent( float currenttime, CChoreoScene *scene, CChoreoEv
 			if ( IsMultiplayer() )
 				break;
 
-			DispatchStartPermitResponses( scene, pActor, event );
+			if ( pActor )
+			{
+				DispatchStartPermitResponses( scene, pActor, event );
+			}
 		}
 		break;
 	default:
@@ -3619,7 +3622,10 @@ void CSceneEntity::EndEvent( float currenttime, CChoreoScene *scene, CChoreoEven
 			if ( IsMultiplayer() )
 				break;
 
-			DispatchEndPermitResponses( scene, pActor, event );
+			if ( pActor )
+			{
+				DispatchEndPermitResponses( scene, pActor, event );
+			}
 		}
 		break;
 	default:
