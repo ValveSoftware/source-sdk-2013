f58b5990206eeda5f73d902aefd6123864cecb05
Peter Covington <petercov@outlook.com>
2022-04-18 17:24:41 -0400

    Fixed custom weapon scripts on client

--- sp/src/game/shared/basecombatweapon_shared.cpp
+++ sp/src/game/shared/basecombatweapon_shared.cpp
@@ -164,7 +164,9 @@ void CBaseCombatWeapon::GiveDefaultAmmo( void )
 //-----------------------------------------------------------------------------
 void CBaseCombatWeapon::Spawn( void )
 {
+#if !defined(CLIENT_DLL) || !defined(MAPBASE)
 	Precache();
+#endif // !defined(CLIENT_DLL) || !defined(MAPBASE)
 
 	BaseClass::Spawn();
 
@@ -239,7 +241,7 @@ const unsigned char *CBaseCombatWeapon::GetEncryptionKey( void )
 void CBaseCombatWeapon::Precache( void )
 {
 #if defined( CLIENT_DLL )
-	Assert( Q_strlen( GetClassname() ) > 0 );
+	Assert( Q_strlen(GetWeaponScriptName() ) > 0 );
 	// Msg( "Client got %s\n", GetClassname() );
 #endif
 	m_iPrimaryAmmoType = m_iSecondaryAmmoType = -1;
@@ -321,7 +323,7 @@ void CBaseCombatWeapon::Precache( void )
 	else
 	{
 		// Couldn't read data file, remove myself
-		Warning( "Error reading weapon data file for: %s\n", GetClassname() );
+		Warning( "Error reading weapon data file for: %s\n", GetWeaponScriptName() );
 	//	Remove( );	//don't remove, this gets released soon!
 	}
 }
