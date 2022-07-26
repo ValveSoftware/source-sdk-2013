9a939547c05daba34e64299324ee7e54a71fd99c
Blixibon <acj3017@gmail.com>
2022-07-26 12:54:01 -0500

    Fixed rare activity translation recursion case

--- sp/src/game/server/ai_basenpc.cpp
+++ sp/src/game/server/ai_basenpc.cpp
@@ -2598,11 +2598,7 @@ bool CAI_BaseNPC::FValidateHintType ( CAI_Hint *pHint )
 Activity CAI_BaseNPC::GetHintActivity( short sHintType, Activity HintsActivity )
 {
 	if ( HintsActivity != ACT_INVALID )
-#ifdef MAPBASE
-		return TranslateActivity( HintsActivity ); // Always translate the activity
-#else
 		return HintsActivity;
-#endif
 
 	return ACT_IDLE;
 }
