319c5083cc444a22cf28d1c9e32d22112fb50934
samisalreadytaken <46823719+samisalreadytaken@users.noreply.github.com>
2024-06-02 21:15:06 +0300

    Fix missing transform in IntersectRayWithOBB

--- sp/src/public/collisionutils.cpp
+++ sp/src/public/collisionutils.cpp
@@ -1475,12 +1475,10 @@ bool IntersectRayWithOBB( const Ray_t &ray, const matrix3x4_t &matOBBToWorld,
 	Collision_ClearTrace( ray.m_Start + ray.m_StartOffset, ray.m_Delta, pTrace );
 
 	// Compute a bounding sphere around the bloated OBB
-	Vector vecOBBCenter;
-	VectorAdd( vecOBBMins, vecOBBMaxs, vecOBBCenter );
-	vecOBBCenter *= 0.5f;
-	vecOBBCenter.x += matOBBToWorld[0][3];
-	vecOBBCenter.y += matOBBToWorld[1][3];
-	vecOBBCenter.z += matOBBToWorld[2][3];
+	Vector vecBoxExtents, vecOBBCenter;
+	VectorAdd( vecOBBMins, vecOBBMaxs, vecBoxExtents );
+	vecBoxExtents *= 0.5f;
+	VectorTransform( vecBoxExtents, matOBBToWorld, vecOBBCenter );
 
 	Vector vecOBBHalfDiagonal;
 	VectorSubtract( vecOBBMaxs, vecOBBMins, vecOBBHalfDiagonal );
