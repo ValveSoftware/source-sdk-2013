d62bfe1fc9be2984da200af61c29b138680453fa
NachosChipeados <103285866+NachosChipeados@users.noreply.github.com>
2025-02-23 11:26:42 -0400

    Add archive flag to convars

--- src/game/shared/tf/tf_viewmodel.cpp
+++ src/game/shared/tf/tf_viewmodel.cpp
@@ -62,8 +62,8 @@ CTFViewModel::~CTFViewModel()
 void DrawEconEntityAttachedModels( CBaseAnimating *pEnt, CEconEntity *pAttachedModelSource, const ClientModelRenderInfo_t *pInfo, int iMatchDisplayFlags );
 
 // TODO:  Turning this off by setting interp 0.0 instead of 0.1 for now since we have a timing bug to resolve
-ConVar cl_wpn_sway_interp( "cl_wpn_sway_interp", "0.0", FCVAR_CLIENTDLL );
-ConVar cl_wpn_sway_scale( "cl_wpn_sway_scale", "5.0", FCVAR_CLIENTDLL );
+ConVar cl_wpn_sway_interp( "cl_wpn_sway_interp", "0.0", FCVAR_CLIENTDLL | FCVAR_ARCHIVE );
+ConVar cl_wpn_sway_scale( "cl_wpn_sway_scale", "5.0", FCVAR_CLIENTDLL | FCVAR_ARCHIVE );
 #endif
 
 //-----------------------------------------------------------------------------
