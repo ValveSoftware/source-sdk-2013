b621f14af549a6ea1d5344e0255c08a9ac87a709
Dmitry Tsarevich <dimhotepus@gmail.com>
2025-02-23 05:02:09 +0300

    bsplib: Ensure WriteBSPFile resets g_pBSPHeader as it points to a local function variable
    Closes #758

--- src/utils/common/bsplib.cpp
+++ src/utils/common/bsplib.cpp
@@ -2738,6 +2738,8 @@ void WriteBSPFile( const char *filename, char *pUnused )
 	g_pFileSystem->Seek( g_hBSPFile, 0, FILESYSTEM_SEEK_HEAD );
 	WriteData( g_pBSPHeader );
 	g_pFileSystem->Close( g_hBSPFile );
+
+	g_pBSPHeader = nullptr;
 }
 
 // Generate the next clear lump filename for the bsp file
