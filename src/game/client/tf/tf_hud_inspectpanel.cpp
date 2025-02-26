94bafd0dbdfdd7d91ba2e713bfee4fea7fe6435c
Sandust60 <96904513+M60TM@users.noreply.github.com>
2025-02-26 11:02:10 +0900

    Move the `IsStealthed` after the `IsSpyDisguisedAsMyTeam`
    - `IsSpyDisguisedAsMyTeam` verifies that the Spy is an enemy

--- src/game/client/tf/tf_hud_inspectpanel.cpp
+++ src/game/client/tf/tf_hud_inspectpanel.cpp
@@ -241,7 +241,7 @@ C_TFPlayer *CHudInspectPanel::GetInspectTarget( C_TFPlayer *pLocalTFPlayer )
 
 			// Preventing players from identifying a cloaked enemy spy through inspection
 			// Fix up if it's a spy disguised as my team
-			if ( !pTargetPlayer->m_Shared.IsStealthed() && pLocalTFPlayer->m_Shared.IsSpyDisguisedAsMyTeam( pTargetPlayer ) )
+			if ( pLocalTFPlayer->m_Shared.IsSpyDisguisedAsMyTeam( pTargetPlayer ) && !pTargetPlayer->m_Shared.IsStealthed() )
 			{
 				// Get the player that the spy is disguised as
 				C_TFPlayer *pDisguiseTarget = pTargetPlayer->m_Shared.GetDisguiseTarget();
