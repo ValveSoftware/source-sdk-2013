00939cd33ee8ab57e3160640d21a07327611e8d2
Your Name <40523890+Brain-dawg@users.noreply.github.com>
2025-03-04 12:53:26 -0500

    formatting

--- src/game/shared/vscript_shared.cpp
+++ src/game/shared/vscript_shared.cpp
@@ -105,7 +105,7 @@ HSCRIPT VScriptCompileScript( const char *pszScriptName, bool bWarnMissing )
 		bool bResult = false;
 
 		// ignore map-packed serverspawn files, allows server owners to run scripts before the map
-		if ( V_strcmp( pszScriptName, "serverspawn" ) == 0 || V_strcmp( pszScriptName, CFmtStr("serverspawn%s", pszVMExtension) ) == 0 )
+		if ( V_strcmp( pszScriptName, "serverspawn" ) == 0 || V_strcmp( pszScriptName, CFmtStr( "serverspawn%s", pszVMExtension ) ) == 0 )
 		{
 			bResult = filesystem->ReadFile( scriptPath, "MOD", bufferScript );
 		}
