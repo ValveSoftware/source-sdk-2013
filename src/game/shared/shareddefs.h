5e8bbd79d6329e8e4babdba292d290f4e3cf8f20
blackletum <blackletum@hotmail.com>
2025-03-11 21:35:13 -0400

    [TF2] Change max FOV from 90 to 130
    This PR is made to increase the max field of view within Team Fortress 2 from 90 to 130.
    This would help players on standard widescreen monitors be able to see as much as players on ultrawide displays, help reduce nausea, to have parity with Valve's other titles such as Left 4 Dead 2 as well as the Team Fortress modding communities typically allowed max FOV settings.

--- src/game/shared/shareddefs.h
+++ src/game/shared/shareddefs.h
@@ -271,7 +271,7 @@ inline bool IsIndexIntoPlayerArrayValid( int iIndex )
 
 #define MAX_PLACE_NAME_LENGTH		18
 
-#define MAX_FOV						90
+#define MAX_FOV						130
 
 //===================================================================================================================
 // Team Defines
