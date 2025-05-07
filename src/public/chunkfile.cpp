33e886c10cfc8af2cdd135b5a2a70555894845b7
clubby789 <jamie@hill-daniel.co.uk>
2025-05-07 11:56:53 +0100

    Fix `CChunkHandlerMap::GetHandler` returning a boolean where a pointer was expected

--- src/public/chunkfile.cpp
+++ src/public/chunkfile.cpp
@@ -135,7 +135,7 @@ ChunkErrorHandler_t CChunkHandlerMap::GetErrorHandler(void **ppData)
 // Input  : pszChunkName - Name of chunk.
 //			ppfnHandler - Receives the address of the callback function.
 //			ppData - Receives the context data for the given chunk.
-// Output : Returns true if a handler was found, false if not.
+// Output : Returns the handler if found, or nullptr if not.
 //-----------------------------------------------------------------------------
 ChunkHandler_t CChunkHandlerMap::GetHandler(const char *pszChunkName, void **ppData)
 {
@@ -151,7 +151,7 @@ ChunkHandler_t CChunkHandlerMap::GetHandler(const char *pszChunkName, void **ppD
 		pNode = pNode->pNext;
 	}
 
-	return(false);
+	return nullptr;
 }
 
 
