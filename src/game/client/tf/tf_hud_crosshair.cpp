172165948bb0c53bbd54721b22b8a2045f07a12c
kindredtf <jackevans1020@gmail.com>
2025-05-09 14:39:46 +0100

    Fix: changed default crosshair scales to 31
    Scale 32 causes non-pixel-perfect rendering, defaults should be
    31 to get 1:1 rendering of the vtfs. Not sure if this applies to
    2160p or 4k but the overwhelming majority play on 1080p where this
    does definitely apply.
    
    I tried to fix this properly but the math kicked my ass.

--- src/game/client/tf/tf_hud_crosshair.cpp
+++ src/game/client/tf/tf_hud_crosshair.cpp
@@ -28,7 +28,7 @@
 
 ConVar cl_crosshair_file( "cl_crosshair_file", "", FCVAR_ARCHIVE );
 
-ConVar cl_crosshair_scale( "cl_crosshair_scale", "32.0", FCVAR_ARCHIVE );
+ConVar cl_crosshair_scale( "cl_crosshair_scale", "31.0", FCVAR_ARCHIVE );
 
 void OnCrosshairColorChanged( IConVar *var, const char *pOldValue, float flOldValue );
 ConVar cl_crosshair_color( "cl_crosshair_color", "200 200 200", FCVAR_ARCHIVE, "Crosshair color in RGB format (\"r g b\")", OnCrosshairColorChanged );
@@ -36,7 +36,7 @@ ConVar pf_ballindicator( "pf_ballindicator", "1", FCVAR_ARCHIVE, "Enable/disable
 ConVar pf_ballindicator_file( "pf_ballindicator_file", "vgui/crosshairs/ballindicator", FCVAR_ARCHIVE, "Change the material for the HUD indicator when holding the ball." );
 ConVar pf_ballindicator_color( "pf_ballindicator_color", "255 255 255", FCVAR_ARCHIVE, "Change the color of the HUD indicator when holding the ball.", OnCrosshairColorChanged );
 ConVar pf_ballindicator_teamcolored( "pf_ballindicator_teamcolored", "1", FCVAR_ARCHIVE, "Overrides the color of the HUD indicator when holding the ball to always use your team's color." );
-ConVar pf_ballindicator_scale( "pf_ballindicator_scale", "32.0", FCVAR_ARCHIVE, "Scale factor of the HUD indicator when holding the ball." );
+ConVar pf_ballindicator_scale( "pf_ballindicator_scale", "31.0", FCVAR_ARCHIVE, "Scale factor of the HUD indicator when holding the ball." );
 using namespace vgui;
 
 // Everything else is expecting to find "CHudCrosshair"
