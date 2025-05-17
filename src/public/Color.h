05ccbf993e34695cc00dafa80cc47c33781bdd8e
Bitl <bitlblox@gmail.com>
2025-05-17 13:18:59 -0700

    improve formatting, add better alpha support

--- src/public/Color.h
+++ src/public/Color.h
@@ -34,19 +34,19 @@ public:
 	{
 		SetColor(_r, _g, _b, _a);
 	}
-	Color(const char *_hexCode, bool alphaHex = false)
+	Color(const char *_hexCode)
 	{
-		int r = V_nibble(_hexCode[0]) << 4 | V_nibble(_hexCode[1]);
-		int g = V_nibble(_hexCode[2]) << 4 | V_nibble(_hexCode[3]);
-		int b = V_nibble(_hexCode[4]) << 4 | V_nibble(_hexCode[5]);
-		int a = 255;
+		int r = V_nibble( _hexCode[0] ) << 4 | V_nibble( _hexCode[1] );
+		int g = V_nibble( _hexCode[2] ) << 4 | V_nibble( _hexCode[3] );
+		int b = V_nibble( _hexCode[4] ) << 4 | V_nibble( _hexCode[5] );
+		int a = 0;
 
-		if (alphaHex)
+		if ( _hexCode[6] && _hexCode[7] )
 		{
-			a = V_nibble(_hexCode[6]) << 4 | V_nibble(_hexCode[7]);
+			a = V_nibble( _hexCode[6] ) << 4 | V_nibble( _hexCode[7] );
 		}
 
-		SetColor(r, g, b, a);
+		SetColor( r, g, b, a );
 	}
 	
 	// set the color
