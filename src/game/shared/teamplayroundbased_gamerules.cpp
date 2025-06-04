1dbad8f50a99c4103ed8726f3d7a864aeb5f6c60
nemmy <panda@pandaptable.moe>
2025-06-04 11:22:31 -0400

    update to bitfield

--- src/game/shared/teamplayroundbased_gamerules.cpp
+++ src/game/shared/teamplayroundbased_gamerules.cpp
@@ -3464,9 +3464,10 @@ float CTeamplayRoundBasedRules::GetRespawnWaveMaxLength( int iTeam, bool bScaleW
 	if ( State_Get() != GR_STATE_RND_RUNNING )
 		return 0;
 
+	// Legacy support to respawn everyone
 	if ( mp_disable_respawn_times.GetInt() == 1 )
 		return 0.0f;
-	else if ( mp_disable_respawn_times.GetInt() >= FIRST_GAME_TEAM && mp_disable_respawn_times.GetInt() == iTeam )
+	if ( mp_disable_respawn_times.GetInt() & (1 << iTeam) )
 		return 0.0f;
 
 	//Let's just turn off respawn times while players are messing around waiting for the tournament to start
