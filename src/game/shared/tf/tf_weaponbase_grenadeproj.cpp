a2e93eefd948ef8a483fdfad84745a6b2b61886a
CarlJosell <carl.josell@gmail.com>
2025-07-25 20:09:01 +0200

    Added pipe timer
    Sticky det sound just before pipe explodes. Server sided and 3d.

--- src/game/shared/tf/tf_weaponbase_grenadeproj.cpp
+++ src/game/shared/tf/tf_weaponbase_grenadeproj.cpp
@@ -20,6 +20,7 @@
 #include "Sprite.h"
 #include "tf_fx.h"
 #include "halloween/merasmus/merasmus.h"
+#include "soundenvelope.h"
 #endif
 
 // memdbgon must be the last include file in a .cpp file!!!
@@ -147,6 +148,7 @@ float CTFWeaponBaseGrenadeProj::GetDamageRadius()
 void CTFWeaponBaseGrenadeProj::Precache( void )
 {
 	BaseClass::Precache();
+	PrecacheScriptSound( "Passtime.PipeBallTimer" );
 
 #ifndef CLIENT_DLL
 	PrecacheModel( NOGRENADE_SPRITE );
@@ -291,6 +293,9 @@ void CTFWeaponBaseGrenadeProj::Spawn( void )
 
 	// Setup the think and touch functions (see CBaseEntity).
 	SetThink( &CTFWeaponBaseGrenadeProj::DetonateThink );
+	CReliableBroadcastRecipientFilter filter;
+		soundQueuePipeTimer = CSoundEnvelopeController::GetController().SoundCreate( 
+			filter, entindex(), "Passtime.PipeBallTimer" );
 	SetNextThink( gpGlobals->curtime + 0.2 );
 }
 
@@ -450,8 +455,12 @@ void CTFWeaponBaseGrenadeProj::DetonateThink( void )
 		return;
 	}
 
-
-	SetNextThink( gpGlobals->curtime + 0.2 );
+	if ( gpGlobals->curtime > m_flDetonateTime - 0.4f && gpGlobals->curtime <= m_flDetonateTime - 0.3f)
+	{
+		CSoundEnvelopeController::GetController().Play( soundQueuePipeTimer, 1, PITCH_NORM);
+	}
+	
+	SetNextThink( gpGlobals->curtime + 0.1 );
 }
 
 //-----------------------------------------------------------------------------
@@ -459,6 +468,11 @@ void CTFWeaponBaseGrenadeProj::DetonateThink( void )
 //-----------------------------------------------------------------------------
 void CTFWeaponBaseGrenadeProj::Detonate( void )
 {
+	if ( soundQueuePipeTimer )
+	{
+		CSoundEnvelopeController::GetController().SoundDestroy( soundQueuePipeTimer );
+	}
+
 	trace_t		tr;
 	Vector		vecSpot;// trace starts here!
 
