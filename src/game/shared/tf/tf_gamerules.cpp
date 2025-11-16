bc48382e624146bbf48cb465d6a258e05d798bd6
Emelyanov Igor <igo1556197@yandex.ru>
2025-11-16 22:43:37 +0300

    Update tf_gamerules.cpp
    Replaced strcmp with Q_strcmp like in the rest of the code. Thanks to @brokenparts for the tip.

--- src/game/shared/tf/tf_gamerules.cpp
+++ src/game/shared/tf/tf_gamerules.cpp
@@ -10581,7 +10581,7 @@ void EconItemInterface_OnOwnerKillEaterEvent( IEconItemInterface *pEconEntity, C
 		if ( !pWearableItem->GetAttributeContainer() )
 			continue;
 
-		if ( !strcmp( pWearableItem->GetAttributeContainer()->GetItem()->GetStaticData()->GetItemClass(), "tf_wearable_demoshield" ) )
+		if ( !Q_strcmp( pWearableItem->GetAttributeContainer()->GetItem()->GetStaticData()->GetItemClass(), "tf_wearable_demoshield" ) )
 			continue;
 
 		EconEntity_ValidateAndSendStrangeMessageToGC( pWearableItem->GetAttributeContainer()->GetItem(), pOwner, pVictim, eEventType, nIncrementValue );
