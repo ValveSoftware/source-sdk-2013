0ae8f67c438df34c193ea9a5649fe4d94e9ec633
copperpixel <48454166+copperpixel@users.noreply.github.com>
2025-11-28 19:35:44 +0100

    tf: fix only r_hand_grip resetting when setting poseparams from econ item schema

--- src/game/client/tf/vgui/tf_playermodelpanel.cpp
+++ src/game/client/tf/vgui/tf_playermodelpanel.cpp
@@ -715,6 +715,7 @@ void CTFPlayerModelPanel::SwitchHeldItemTo( CEconItemView *pItem )
 	}
 
 	// update poseparam
+	SetPoseParameters( NULL, 0 );
 	if ( pItem->GetStaticData()->GetNumPlayerPoseParameters( m_iTeam ) > 0 )
 	{
 		for ( int iPlayerPoseParam=0; iPlayerPoseParam < pItem->GetStaticData()->GetNumPlayerPoseParameters( m_iTeam ); ++iPlayerPoseParam )
@@ -722,10 +723,6 @@ void CTFPlayerModelPanel::SwitchHeldItemTo( CEconItemView *pItem )
 			poseparamtable_t *pPoseParam = pItem->GetStaticData()->GetPlayerPoseParameters( m_iTeam, iPlayerPoseParam );
 			SetPoseParameterByName( pPoseParam->strName, pPoseParam->flValue );
 		}
-	}
-	else
-	{
-		SetPoseParameterByName( "r_hand_grip", 0.f );
 	}
 
 	// Clear out taunt particles
