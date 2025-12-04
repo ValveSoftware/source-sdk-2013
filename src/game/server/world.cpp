60309e5343cd2b246d76bdf5674c5690c625f9bb
Yui <50331474+SirYodaJedi@users.noreply.github.com>
2025-12-04 10:30:00 -0500

    Make infodecal and info_projecteddecal serverside as well They are networked separately of the edict, via temporary entities

--- src/game/server/world.cpp
+++ src/game/server/world.cpp
@@ -43,10 +43,10 @@ extern CUtlMemoryPool g_EntityListPool;
 
 #define SF_DECAL_NOTINDEATHMATCH		2048
 
-class CDecal : public CPointEntity
+class CDecal : public CServerOnlyPointEntity
 {
 public:
-	DECLARE_CLASS( CDecal, CPointEntity );
+	DECLARE_CLASS( CDecal, CServerOnlyPointEntity );
 
 	void	Spawn( void );
 	bool	KeyValue( const char *szKeyName, const char *szValue );
@@ -240,10 +240,10 @@ bool CDecal::KeyValue( const char *szKeyName, const char *szValue )
 //-----------------------------------------------------------------------------
 // Purpose: Projects a decal against a prop
 //-----------------------------------------------------------------------------
-class CProjectedDecal : public CPointEntity
+class CProjectedDecal : public CServerOnlyPointEntity
 {
 public:
-	DECLARE_CLASS( CProjectedDecal, CPointEntity );
+	DECLARE_CLASS( CProjectedDecal, CServerOnlyPointEntity );
 
 	void	Spawn( void );
 	bool	KeyValue( const char *szKeyName, const char *szValue );
