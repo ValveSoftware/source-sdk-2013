6e5dca27810ede16de1f0397e9f75b46353ccebf
Joey <45763358+JoeyCheerio@users.noreply.github.com>
2025-12-26 17:20:34 -0500

    Update jigglebones.cpp
    Included Ficool's better fix

--- src/public/jigglebones.cpp
+++ src/public/jigglebones.cpp
@@ -588,14 +588,15 @@ void CJiggleBones::BuildJiggleTransformations( int boneIndex, float currenttime,
 
 		data->lastBoingPos = goalBasePosition;
 
-		float speed = vel.NormalizeInPlace();
-		if ( speed < 0.00001f )
+				float speed;
+		if ( vel.IsZero() )
 		{
 			vel = Vector( 0, 0, 1.0f );
 			speed = 0.0f;
 		}
 		else
 		{
+			speed = vel.NormalizeInPlace();
 			speed /= deltaT;
 		}
 
@@ -674,9 +675,6 @@ void CJiggleBones::BuildJiggleTransformations( int boneIndex, float currenttime,
 				boingSide = CrossProduct( data->boingDir, Vector( 0, 0, 1.0f ) );
 			}
 			boingSide.NormalizeInPlace();
-			
-				if (data->boingDir.IsZero()) data->boingDir = Vector(0, 0, 1);
-			if (boingSide.IsZero()) boingSide = Vector(1, 0, 0);
 			
 			Vector boingOtherSide = CrossProduct( data->boingDir, boingSide );
 
