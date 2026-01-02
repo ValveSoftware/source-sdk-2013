c391483466f26c722efae08fdea699c816cb9e81
MasterOfRespawn <104586366+MasterOfRespawn@users.noreply.github.com>
2026-01-02 18:50:52 +0100

    do not try to target disabled capture zones

--- src/game/server/tf/bot/tf_bot.cpp
+++ src/game/server/tf/bot/tf_bot.cpp
@@ -2171,7 +2171,7 @@ CCaptureZone *CTFBot::GetFlagCaptureZone( void ) const
 	for( int i=0; i<ICaptureZoneAutoList::AutoList().Count(); ++i )
 	{
 		CCaptureZone *zone = static_cast< CCaptureZone* >( ICaptureZoneAutoList::AutoList()[i] );
-		if ( zone->GetTeamNumber() != GetEnemyTeam(GetTeamNumber()) )
+		if ( !zone->IsDisabled() && zone->GetTeamNumber() != GetEnemyTeam(GetTeamNumber()) )
 		{
 			return zone;
 		}
