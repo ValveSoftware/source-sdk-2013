9973966073ac5d44663b22fa52d84b693c3616c8
Lunaire <121734810+lunairekitty@users.noreply.github.com>
2026-01-31 23:48:22 -0500

    Adjust ray trace position calculation
    Change the pos value form 0.999 to 0.99 to fix the issue of env_sun disappearing when directly under the crosshair, even when the crosshair sprite is not active.

--- src/game/client/glow_overlay.cpp
+++ src/game/client/glow_overlay.cpp
@@ -159,7 +159,7 @@ void CGlowOverlay::UpdateSkyGlowObstruction( float zFar, bool bCacheFullSceneSta
 	if ( PixelVisibility_IsAvailable() )
 	{
 		// Trace a ray at the object. 
-		Vector pos = CurrentViewOrigin() + m_vDirection * zFar * 0.999f;
+		Vector pos = CurrentViewOrigin() + m_vDirection * zFar * 0.99f;
 
 		// UNDONE: Can probably do only the pixelvis query in this case if you can figure out where
 		// to put it - or save the position of this trace
