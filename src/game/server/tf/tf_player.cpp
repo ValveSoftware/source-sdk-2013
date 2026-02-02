703afd754c68132ebe4feeb054118ee1f15b92e3
paypur <pieceofpaypur@gmail.com>
2026-02-02 04:24:15 -0500

    Fixed restoring drained health removing overheal while not actively being healed

--- src/game/server/tf/tf_player.cpp
+++ src/game/server/tf/tf_player.cpp
@@ -1117,6 +1117,7 @@ CTFPlayer::CTFPlayer()
 
 	m_bViewingCYOAPDA = false;
 
+	m_flLastOverhealMultiplier = 1.f;
 	ResetMaxHealthDrain();
 
 	m_bRegenerating = false;
@@ -14267,6 +14268,11 @@ int CTFPlayer::GetMaxHealthForBuffing()
 	int iMax = m_PlayerClass.GetMaxHealth();
 	CALL_ATTRIB_HOOK_INT( iMax, add_maxhealth );
 
+	if (m_Shared.GetNumHealers() > 0)
+	{
+		m_flLastOverhealMultiplier = m_Shared.GetMaxOverhealMultiplier();
+	}
+	
 	CTFWeaponBase *pWeapon = GetActiveTFWeapon();
 	if ( pWeapon )
 	{
@@ -14402,7 +14408,7 @@ int CTFPlayer::GetMaxHealthForBuffing()
 				if ( nHealthRestore > 0 )
 				{
 					m_dMaxHealthDrainHealthAccumulator -= nHealthRestore;
-					int nHealthMaxOverheal = nOriginalMaxHealth * m_Shared.GetMaxOverhealMultiplier();
+					int nHealthMaxOverheal = nOriginalMaxHealth * m_flLastOverhealMultiplier;
 					int nHealthMaxAttribute = nOriginalMaxHealth;
 					CALL_ATTRIB_HOOK_INT( nHealthMaxAttribute, add_maxhealth_nonbuffed );
 					int nHealthMax = Max( nHealthMaxOverheal, nHealthMaxAttribute );
