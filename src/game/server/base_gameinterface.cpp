5288cef18fac3d131da546ffc3aced82c689c7ab
pivotman319 <pivotman2003@gmail.com>
2026-02-26 16:20:22 +0100

    better maxplayers limit preprocessor var detection for x86 and amd64 platforms

--- src/game/server/base_gameinterface.cpp
+++ src/game/server/base_gameinterface.cpp
@@ -11,29 +11,31 @@
 // memdbgon must be the last include file in a .cpp file!!!
 #include "tier0/memdbgon.h"
 
-void CServerGameClients::GetPlayerLimits( int& minplayers, int& maxplayers, int &defaultMaxPlayers ) const
+void CServerGameClients::GetPlayerLimits(int& minplayers, int& maxplayers, int& defaultMaxPlayers) const
 {
 	minplayers = defaultMaxPlayers = 1;
 
 	// Pivot (25/02/2026): If targeting amd64 platforms AND conditional UNRESTRICTED_MAXPLAYERS_ENABLE
 	// set to 1 in game project vpc, raise to MAX_PLAYERS limit specified in game/shared/shareddefs.h.
-	
+
 	// Code adapted from game/server/tf/tf_gameinterface.cpp
-#if defined( PLATFORM_64BITS ) && defined( UNRESTRICTED_MAXPLAYERS )	// Assuming amd64
+#ifdef UNRESTRICTED_MAXPLAYERS
+#ifdef PLATFORM_64BITS	// Assuming amd64
 	maxplayers = MAX_PLAYERS;
-#elif !defined ( PLATFORM_64BITS ) && defined( UNRESTRICTED_MAXPLAYERS )	// Assuming x86/i686
-	if ( CommandLine()->HasParm("-unrestricted_maxplayers") )
+#else	// Assuming x86/i686
+	if (CommandLine()->HasParm("-unrestricted_maxplayers"))
 	{
 		static bool s_bWarned = false;
-		if ( !s_bWarned )
+		if (!s_bWarned)
 		{
-			Warning( "The use of -unrestricted_maxplayers is NOT supported and definitely NOT recommended and may be unstable.\n" );
+			Warning("The use of -unrestricted_maxplayers is NOT supported and definitely NOT recommended and may be unstable.\n");
 			s_bWarned = true;
 		}
 		maxplayers = MAX_PLAYERS;
 	}
 	else
 		maxplayers = 33;	// Accounting for one extra SourceTV client.
+#endif
 #else 
 	maxplayers = MAX_PLAYERS;	// MAX_PLAYERS = 33
 #endif
@@ -44,6 +46,6 @@ void CServerGameClients::GetPlayerLimits( int& minplayers, int& maxplayers, int 
 // Mod-specific CServerGameDLL implementation.
 // -------------------------------------------------------------------------------------------- //
 
-void CServerGameDLL::LevelInit_ParseAllEntities( const char *pMapEntities )
+void CServerGameDLL::LevelInit_ParseAllEntities(const char* pMapEntities)
 {
 }
--- src/game/server/hl2mp/hl2mp_gameinterface.cpp
+++ src/game/server/hl2mp/hl2mp_gameinterface.cpp
@@ -23,10 +23,11 @@ void CServerGameClients::GetPlayerLimits( int& minplayers, int& maxplayers, int 
 	
 	// Pivot (25/02/2026): If targeting amd64 platforms AND conditional UNRESTRICTED_MAXPLAYERS_ENABLE
 	// set to 1 in game project vpc, raise to MAX_PLAYERS limit specified in game/shared/shareddefs.h.
-#if defined( PLATFORM_64BITS ) && defined( UNRESTRICTED_MAXPLAYERS )	// Assuming amd64
+#ifdef UNRESTRICTED_MAXPLAYERS 
+#ifdef PLATFORM_64BITS 	// Assuming amd64
 	maxplayers = MAX_PLAYERS;
-#elif !defined ( PLATFORM_64BITS ) && defined( UNRESTRICTED_MAXPLAYERS )	// Assuming x86/i686
-	if ( CommandLine()->HasParm("-unrestricted_maxplayers") )
+#else	// Assuming x86/i686
+	if ( CommandLine()->HasParm( "-unrestricted_maxplayers" ) )
 	{
 		static bool s_bWarned = false;
 		if ( !s_bWarned )
@@ -38,6 +39,7 @@ void CServerGameClients::GetPlayerLimits( int& minplayers, int& maxplayers, int 
 	}
 	else
 		maxplayers = 33;	// Accounting for one extra SourceTV client.
+#endif
 #else 
 	maxplayers = MAX_PLAYERS;	// MAX_PLAYERS = 33
 #endif
