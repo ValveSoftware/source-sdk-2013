a66b79007fa7a762d55c72ccd2436464a8021796
naydef <naydel9@hotmail.com>
2026-05-02 16:31:39 +0000

    Show TF2 style hud messages even if cl_hud_minmode is 1
    These look nice, but cannot be shown to players in min mode. 
    
    This necessitate a fallback code in SM plugins or repeating the same information with the other options (chat, hudText).
    
    So make them visible irrespective of cl_hud_minmode

--- src/game/client/tf/tf_hud_notification_panel.cpp
+++ src/game/client/tf/tf_hud_notification_panel.cpp
@@ -139,11 +139,6 @@ void CHudNotificationPanel::MsgFunc_HudNotify( bf_read &msg )
 //-----------------------------------------------------------------------------
 void CHudNotificationPanel::MsgFunc_HudNotifyCustom( bf_read &msg )
 {
-	// Ignore notifications in minmode
-	ConVarRef cl_hud_minmode( "cl_hud_minmode", true );
-	if ( cl_hud_minmode.IsValid() && cl_hud_minmode.GetBool() )
-		return;
-
 	// Reload the base
 	LoadControlSettings( "resource/UI/notifications/base_notification.res" );
 
