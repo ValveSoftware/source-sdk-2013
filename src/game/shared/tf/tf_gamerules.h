52d244db549c70c01ac743afa8b9ff40bdb7dc25
blackletum <blackletum@hotmail.com>
2026-05-03 03:49:55 -0400

    Change default FOV to 90 from 75.

--- src/game/shared/tf/tf_gamerules.h
+++ src/game/shared/tf/tf_gamerules.h
@@ -1036,7 +1036,7 @@ private:
 
 	void ChooseNextMapVoteOptions();
 
-	int DefaultFOV( void ) { return 75; }
+	int DefaultFOV( void ) { return 90; }
 	int GetDuckSkinForClass( int nTeam, int nClass ) const;
 	void MatchSummaryTeleport();
 
