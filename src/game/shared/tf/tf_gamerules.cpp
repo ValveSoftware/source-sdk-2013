c738d1f76f97ce312cc4d336112054d3bd99b225
SolarLight <SolarLightTwo@gmail.com>
2026-07-19 21:35:45 +0100

    disable random fall damage spread by default

--- src/game/shared/tf/tf_gamerules.cpp
+++ src/game/shared/tf/tf_gamerules.cpp
@@ -10705,7 +10705,7 @@ void CTFGameRules::ClientDisconnected( edict_t *pClient )
 // Falling damage stuff.
 #define TF_PLAYER_MAX_SAFE_FALL_SPEED	650		
 
-ConVar tf_fall_damage_disablespread( "tf_fall_damage_disablespread", "0", FCVAR_NONE );
+ConVar tf_fall_damage_disablespread( "tf_fall_damage_disablespread", "1", FCVAR_NONE );
 
 float CTFGameRules::FlPlayerFallDamage( CBasePlayer *pPlayer )
 {
